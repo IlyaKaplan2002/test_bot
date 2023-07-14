6cfc92906f5467f6a6b182d868db05b74954c982
Ilya Kaplan <ik@alphaguilty.io>
2023-07-14 22:12:11 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [browser, setBrowser] = useState(null);
+
+  useEffect(() => {
+    const ua = navigator.userAgent.toLowerCase();
+    if (ua.includes("telegram")) {
+      setBrowser("Telegram");
+      return;
+    }
+    if (ua.includes("whatsapp")) {
+      setBrowser("WhatsApp");
+      return;
+    }
+    if (ua.includes("viber")) {
+      setBrowser("Viber");
+      return;
+    }
+    if (ua.includes("twitter")) {
+      setBrowser("Twitter");
+      return;
+    }
+    if (ua.includes("discord")) {
+      setBrowser("Discord");
+      return;
+    }
+    if (ua.includes("instagram")) {
+      setBrowser("Instagram");
+      return;
+    }
+    if (ua.includes("tiktok")) {
+      setBrowser("TikTok");
+      return;
+    }
+    setBrowser("not in-app");
+  }, []);
+
+  const goToDefault = () =>
+    (window.location.href = `https://${window.location.host}`);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>You are in {browser} browser</p>
+        <p>{navigator.userAgent}</p>
+        {browser !== "not in-app" && (
+          <button type="button" onClick={goToDefault}>
+            Go to default browser
+          </button>
+        )}
       </header>
     </div>
   );
